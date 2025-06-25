07ab31bd5ae1459a9b6f25176ea31ab5c9b9636e
Miggy-py <106600118+Miggy-py@users.noreply.github.com>
2025-06-25 12:38:23 -0400

    Update twttr

--- twttr/twttr.py
+++ twttr/twttr.py
@@ -1,12 +1,12 @@
 def main():
-    user_input = input("Input: ")
+    user_input = input("Input: ").strip()
 
     print("Output: ", end ="")
     print_without_vowels(user_input)
 
 
 def print_without_vowels(user_input):
-    VOWELS = ["a", "e", "i", "o", "u"]
+    VOWELS = {"a", "e", "i", "o", "u"}
 
     for char in user_input:
         if (char.lower() not in VOWELS):
